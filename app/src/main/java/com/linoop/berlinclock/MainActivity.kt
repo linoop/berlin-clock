1fe16a75e8264f3ca256f8271295c9f758ce7410
Linoop Laxman <linoop1989@gmail.com>
2025-07-31 16:17:22 +0530

    feat(ui): create clock screen with top app bar

--- app/src/main/java/com/linoop/berlinclock/MainActivity.kt
+++ app/src/main/java/com/linoop/berlinclock/MainActivity.kt
@@ -9,20 +9,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.linoop.berlinclock.presentation.ui.screens.BerlinClockScreen
 import com.linoop.berlinclock.ui.theme.BerlinClockTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BerlinClockTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        text = "Berlin Clock",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                BerlinClockScreen(hiltViewModel())
             }
         }
     }
